ce32a47dd6466f11214ca63b03d712224139dce2
favour mogaji <favourmogaji3@gmail.com>
2026-05-22 14:44:51 -0500

    maps beginners level

--- index.js
+++ index.js
@@ -5,10 +5,29 @@
 // }
 // greeting()
 
-multiple= (num1) => {
- return num1 * num1
+// multiple= (num1) => {
+//  return num1 * num1
+// }
+
+// console.log(multiple(3));
+
+// greet =() =>{
+//     console.log("Hello welcome");
+// }
+// greet()
+
+// let nums = [2,4,6,8]
+// let result = nums.map((num) =>{
+//     return num * num
+    
+// })
+// console.log(result);
+
+let numbers = [5,15,25,35]
+let answer = numbers.map( (num) =>{
+    return num + 10
+
 }
 
-console.log(multiple(3));
-
-
+)
+console.log(answer)
